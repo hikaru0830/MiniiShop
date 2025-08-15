da927a38ad098abe9009e975f2340ee29c62cc8b
Gwen <hikaru0830@hotmail.com>
2025-08-16 02:11:25 +0800

    chore: remove unused code

--- mns/frontend/src/main.jsx
+++ mns/frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-//DEMO========================================
 import './index.css'
 import App from './app/App.jsx'
 
@@ -10,29 +9,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-//============================================
-// const mainScript = document.getElementById('mainScript');
-// const path = mainScript.dataset.path + '.jsx';
-// const mode = mainScript.dataset.mode;
-// const pages = import.meta.glob('./pages/**/*.jsx');
-
-// if (pages[path]) {
-//     pages[path]().then(module => {
-//         const Component = module.GetView;
-//         createRoot(document.getElementById('root')).render(GetNode(Component))
-//     });
-// }
-
-// function GetNode(Component) {
-//   switch (mode.toUpperCase()) {
-//     case 'DEVELOPMENT':
-//       return (
-//         <StrictMode>
-//           <Component />
-//         </StrictMode>
-//       );
-//     case 'PRODUCTION':
-//     default:
-//       return <Component />
-//   }
-// }
